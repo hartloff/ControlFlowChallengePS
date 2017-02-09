577c977dbc207e4d6f7331b5371b100a886e8cb6
jessehartloff <hartloff@buffalo.edu>
2017-02-09 15:23:31 -0500

    initial commit

---
+++ src/ControlFlowChallengePS.java
@@ -0,0 +1,67 @@
+public class ControlFlowChallengePS {
+
+
+
+    // 2 points
+    static boolean Q1(int number) {
+
+        // TODO: Return true if number is a perfect cube, false otherwise. You can assume the value
+        //       of number is between 1 and 1000000
+        //
+        // Note: a perfect cube is a number whose cube root is an integer
+
+
+
+        return true;
+    }
+
+
+
+    // 3 points
+    static void Q2(int n) {
+        // TODO: print all the perfect squares less than or equal to n
+        //
+        // Example: If n is 25 print "1 4 9 16 25"
+        // (you can print the numbers on separate lines or on the same line)
+
+
+
+    }
+
+
+
+    // 5 points
+    static String Q3(int a, int b, int c, double lowerBound, double targetValue, double upperBound) {
+
+        // TODO: If the average of a, b, and c is less than lower bound, return "too low".
+        //       If the average of a, b, and c is greater than upper bound, return "too high".
+        //       If the average of a, b, and c is within 2.5 of the target value, return "just right".
+        //       If the average of a, b, and c is between the upper and lower bounds,
+        //          but not within 2.5 of the target value, return "almost".
+
+
+
+        return "";
+    }
+
+
+
+    // 5 points
+    static void Q4(int n) {
+        // TODO: print all the prime numbers less than or equal to n
+        // Example: If n is 25 print "2 3 5 7 11 13 17 19 23"
+        // (you can print the numbers on separate lines or on the same line)
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // You can optionally use this main method to test your code without having to submit.
+        // This main method will not be graded
+
+    }
+
+}
